6d14aa909d92b42b38b0cfe6659a28b059a44bde
Wojciech Inglot <wojciech@inglot.net.pl>
2017-12-04 12:18:11 +0100

    Replace log with logrus package

--- main.go
+++ main.go
@@ -1,30 +1,50 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"log"
+	"os"
 
 	"github.com/brainly/olowek/config"
 	"github.com/brainly/olowek/marathon"
 	"github.com/brainly/olowek/utils"
 	"github.com/brainly/olowek/worker"
+	"github.com/jessevdk/go-flags"
+	log "github.com/sirupsen/logrus"
 )
 
 const (
 	DefaultConfigPath = "/etc/olowek/olowek.json"
 )
 
+var opts struct {
+	Config string `short:"c" long:"config" description:"Path to configuration file." default:"/etc/olowek/olowek.json"`
+	Debug  bool   `short:"d" long:"debug" description:"Enable debug logging"`
+}
+
 func main() {
-	cfgFlag := flag.String("c", DefaultConfigPath, fmt.Sprintf("Path to configuration file [default: %s]", DefaultConfigPath))
-	flag.Parse()
+	if _, err := flags.Parse(&opts); err != nil {
+		os.Exit(1)
+	}
 
-	cfg, err := config.NewConfigFromFile(*cfgFlag)
-	panicOnError(err)
+	if opts.Debug {
+		log.SetLevel(log.DebugLevel)
+	}
 
+	cfg, err := config.NewConfigFromFile(opts.Config)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"config": opts.Config,
+			"err":    err,
+		}).Fatal("Error reading configuration from file")
+	}
 	cfg.NginxReloadFunc = utils.NginxReload
 
-	client := setupMarathon(cfg.Marathon)
+	client, err := marathon.NewMarathonClient(cfg.Marathon)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"url": cfg.Marathon,
+			"err": err,
+		}).Fatal("Error creating Marathon client")
+	}
 
 	worker := worker.Worker{
 		Trigger: make(chan bool, 2),
@@ -33,17 +53,4 @@ func main() {
 	worker.Work()
 
 	client.ConnectToEventStream(worker.Trigger)
-}
-
-func setupMarathon(marathonURL string) marathon.Marathon {
-	client, err := marathon.NewMarathonClient(marathonURL)
-	panicOnError(err)
-
-	return client
-}
-
-func panicOnError(err error) {
-	if err != nil {
-		log.Panicf("Panic: %v", err)
-	}
 }
